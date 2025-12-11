eff36d55c8af122e3105410266041806311a5820
Rabia <154100104+rabiicolak@users.noreply.github.com>
2025-12-11 15:00:48 +0300

    Create coordinates.py

---
+++ data/coordinates.py
@@ -0,0 +1,14 @@
+# Isparta Acil Toplanma Alanları - Koordinatlar
+
+locations = [
+    {"name": "Çünür - Üniversite Cad. Park Alanı", "lat": 37.78422, "lng": 30.56492},
+    {"name": "Çünür - Gülkent Kapalı Pazar", "lat": 37.78460, "lng": 30.55390},
+    {"name": "Modernevler Kapalı Pazaryeri", "lat": 37.77450, "lng": 30.55170},
+    {"name": "Işıkkent - Mehmet Tönge Parkı", "lat": 37.77080, "lng": 30.54940},
+    {"name": "Karaağaç - Kaymakkapı Meydanı", "lat": 37.76610, "lng": 30.55230},
+    {"name": "Pirimehmet - 15 Temmuz Meydanı", "lat": 37.76540, "lng": 30.55280},
+    {"name": "Bahçelievler Pazar Yeri", "lat": 37.76080, "lng": 30.54410},
+    {"name": "Sanayi Mah. Otoparkı", "lat": 37.76950, "lng": 30.53680}
+]
+
+n_cities = len(locations)
